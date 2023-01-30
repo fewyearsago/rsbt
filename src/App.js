f50fa4d1fb58485bbb775b534fbb22ff4ccc181d
zaurbkv <zaurbkv@mail.ru>
2023-01-31 00:09:42 +0300

    dsadssd

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './styles/reset.css';
 import Main from './pages/Main';
 import About from './pages/About';
 import { Routes, Route } from 'react-router-dom';
+import { animated, useSpring } from '@react-spring/web';
 
 function App() {
+  const [props, api] = useSpring(
+    () => ({
+      from: { opacity: 0 },
+      to: { opacity: 1 },
+    }),
+    [],
+  );
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
+      <animated.div style={props}>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </animated.div>
     </div>
   );
 }
