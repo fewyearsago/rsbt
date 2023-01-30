3eacc349a179fd3d1ce0a7585caee53c362cbe9f
zaurbkv <zaurbkv@mail.ru>
2023-01-30 17:41:55 +0300

    sdassa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './styles/styles.scss';
 import './styles/reset.css';
 import Main from './pages/Main';
+import About from './pages/About';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
