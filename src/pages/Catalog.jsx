a15f0b330652a231045aecdf2003c45911acce8c
Magomed <zaurbkv@mail.ru>
2023-02-07 04:02:09 +0300

    aafsdasdasd

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Menu from '../components/Menu';
 
 const Catalog = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Catalog;
